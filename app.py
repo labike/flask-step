12bb97dcce110668eba5689f0a16ceb82f4d14b6
labike <ddmmy@hotmail.com>
2023-09-11 22:14:28 +0800

    two step practice jinja2

--- app.py
+++ app.py
@@ -3,6 +3,27 @@ from flask import Flask, request, render_template, redirect, url_for, Response, 
 import config
 from werkzeug.routing import BaseConverter
 
+movies = [
+    {
+        'id': '123',
+        'thumbling': 'https://gimg3.baidu.com/search/src=http%3A%2F%2Fpics1.baidu.com%2Ffeed%2Fdc54564e9258d109dad324a26e0b09b36c814d4a.jpeg%40f_auto%3Ftoken%3D921adfeb82738a8511abbfe7689b413c&refer=http%3A%2F%2Fwww.baidu.com&app=2021&size=f360,240&n=0&g=0n&q=75&fmt=auto?sec=1694538000&t=9be706d634eeea468f0087e53d922994',
+        'title': '吃了吗',
+        'rating': '1.2',
+        'comment_count': 100,
+        'authors': 'heihei'
+    }
+]
+tvs = [
+    {
+        'id': '234',
+        'thumbling': 'https://gimg3.baidu.com/search/src=http%3A%2F%2Fpics7.baidu.com%2Ffeed%2Feaf81a4c510fd9f9e5e0f80e9a7e11262934a446.jpeg%40f_auto%3Ftoken%3D025a36e78152a33cb976d044249ffa41&refer=http%3A%2F%2Fwww.baidu.com&app=2021&size=f360,240&n=0&g=0n&q=75&fmt=auto?sec=1694538000&t=90f86f28c77c896d5367c3495173ef0b',
+        'title': '李光洁',
+        'rating': '90',
+        'comment_count': 100,
+        'authors': 'haha'
+    }
+]
+
 app = Flask(__name__)
 # app.config.from_object(config)
 app.config.from_pyfile('config.py', silent=True)
@@ -117,6 +138,24 @@ def count():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/douban')
+def douban():
+    context = {
+        'movies': movies,
+        'tvs': tvs
+    }
+    return render_template('douban.html', **context)
+
+@app.route('/douban_list')
+def douban_list():
+    category = int(request.args.get('category'))
+    items = None
+    if category == 1:
+        items = movies
+    else:
+        items = tvs
+    return render_template('list.html', items = items)
 
 if __name__ == '__main__':
     app.run()
